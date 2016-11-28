9491227f85bc5d5310836347a880ccd0d3d2f5dc
Pavel Lechev <pavel@schoolclubz.com>
2016-11-28 22:10:52 +0000

    Refactor tests to reflect assignment example (1-20); Fix typo "fuzz" -> "fizz"

--- src/main/scala/FuzzBuzzPrinter.scala
+++ src/main/scala/FuzzBuzzPrinter.scala
@@ -4,13 +4,13 @@
   * @author Pavel Lechev <p.lechev@gmail.com>
   * @since 28/11/2016
   */
-object FuzzBuzzPrinter {
+object FizzBuzzPrinter {
 
   def getOutput(range: List[Int]): String = range.map { n =>
     n match {
       case v if v.toString.contains('3') => "lucky"
-      case v if v % 15 == 0 => "fuzzbuzz" // at the top to avoid being caught by 3 & 5 multiples
-      case v if v % 3 == 0 => "fuzz"
+      case v if v % 15 == 0 => "fizzbuzz" // at the top to avoid being caught by 3 & 5 multiples
+      case v if v % 3 == 0 => "fizz"
       case v if v % 5 == 0 => "buzz"
       case _ => n
     }
--- src/test/scala/FuzzBuzzPrinterSpec.scala
+++ src/test/scala/FuzzBuzzPrinterSpec.scala
@@ -1,50 +1,50 @@
 import org.scalatest._
 
-class FuzzBuzzPrinterSpec extends FlatSpec with Matchers {
+class FizzBuzzPrinterSpec extends FlatSpec with Matchers {
 
-  "FuzzBuzzPrinter" should """print a number for non multiples of 3, 5 or 15""" in {
-    val output = FuzzBuzzPrinter.getOutput(List(1,2,4,7,8,11,13,14,16,17))
+  "FizzBuzzPrinter" should """print a number for non multiples of 3, 5 or 15""" in {
+    val output = FizzBuzzPrinter.getOutput(List(1,2,4,7,8,11,13,14,16,17))
     output shouldEqual "1 2 4 7 8 11 13 14 16 17"
   }
 
-  it should """print "fuzz" for multiples of 3""" in {
-    val output = FuzzBuzzPrinter.getOutput((1 to 7).toList)
-    output shouldEqual "1 2 fuzz 4 buzz fuzz 7"
+  it should """print "fizz" for multiples of 3""" in {
+    val output = FizzBuzzPrinter.getOutput((1 to 7).toList)
+    output shouldEqual "1 2 fizz 4 buzz fizz 7"
   }
 
-  it should """print "fuzz" for multiples of 3 only""" in {
-    val output = FuzzBuzzPrinter.getOutput(List(3,6,9,12,18))
-    output shouldEqual "fuzz fuzz fuzz fuzz fuzz"
+  it should """print "fizz" for multiples of 3 only""" in {
+    val output = FizzBuzzPrinter.getOutput(List(3,6,9,12,18))
+    output shouldEqual "fizz fizz fizz fizz fizz"
   }
 
   it should """print "buzz" for multiples of 5""" in {
-    val output = FuzzBuzzPrinter.getOutput((1 to 10).toList)
-    output shouldEqual "1 2 fuzz 4 buzz fuzz 7 8 fuzz buzz"
+    val output = FizzBuzzPrinter.getOutput((1 to 10).toList)
+    output shouldEqual "1 2 fizz 4 buzz fizz 7 8 fizz buzz"
   }
 
   it should """print "buzz" for multiples of 5 only""" in {
-    val output = FuzzBuzzPrinter.getOutput(List(5, 10, 20, 25, 35))
+    val output = FizzBuzzPrinter.getOutput(List(5, 10, 20, 25, 35))
     output shouldEqual "buzz buzz buzz buzz buzz"
   }
 
-  it should """print "fuzzbuzz" for multiples of 15""" in {
-    val output = FuzzBuzzPrinter.getOutput((1 to 17).toList)
-    output shouldEqual "1 2 fuzz 4 buzz fuzz 7 8 fuzz buzz 11 fuzz 13 14 fuzzbuzz 16 17"
+  it should """print "fizzbuzz" for multiples of 15""" in {
+    val output = FizzBuzzPrinter.getOutput((1 to 20).toList)
+    output shouldEqual "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz"
   }
 
-  it should """print "fuzzbuzz" for multiples of 15 only""" in {
-    val output = FuzzBuzzPrinter.getOutput(List(15, 30, 45, 60))
-    output shouldEqual "fuzzbuzz fuzzbuzz fuzzbuzz fuzzbuzz"
+  it should """print "fizzbuzz" for multiples of 15 only""" in {
+    val output = FizzBuzzPrinter.getOutput(List(15, 30, 45, 60))
+    output shouldEqual "fizzbuzz fizzbuzz fizzbuzz fizzbuzz"
   }
 
-  it should """print "fuzz" "buzz" and "fuzzbuzz" for random multiples of 3, 5 and 15 only""" in {
-    val output = FuzzBuzzPrinter.getOutput(List(3, 6, 5, 12, 15, 30, 45, 60, 10))
-    output shouldEqual "fuzz fuzz buzz fuzz fuzzbuzz fuzzbuzz fuzzbuzz fuzzbuzz buzz"
+  it should """print "fizz" "buzz" and "fizzbuzz" for random multiples of 3, 5 and 15 only""" in {
+    val output = FizzBuzzPrinter.getOutput(List(3, 6, 5, 12, 15, 30, 45, 60, 10))
+    output shouldEqual "fizz fizz buzz fizz fizzbuzz fizzbuzz fizzbuzz fizzbuzz buzz"
   }
 
   it should """print "lucky" if number contains the digit '3' """ in {
-    val output = FuzzBuzzPrinter.getOutput((1 to 14).toList)
-    output shouldEqual "1 2 lucky 4 buzz fuzz 7 8 fuzz buzz 11 fuzz lucky 14"
+    val output = FizzBuzzPrinter.getOutput((1 to 20).toList)
+    output shouldEqual "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz"
   }
 
 
