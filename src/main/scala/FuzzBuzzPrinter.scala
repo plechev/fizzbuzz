eaf70aa724f7ab0578851b5b3635a10b52dd0d51
Pavel Lechev <pavel@schoolclubz.com>
2016-11-28 21:26:53 +0000

    Basic implementation print numbers only - test passing

---
+++ src/main/scala/FuzzBuzzPrinter.scala
@@ -0,0 +1,14 @@
+
+
+/**
+  * @author Pavel Lechev <p.lechev@gmail.com>
+  * @since 28/11/2016
+  */
+object FuzzBuzzPrinter {
+
+  def getOutput(range: List[Int]) = range mkString " "
+
+
+}
+
+
--- src/test/scala/FuzzBuzzPrinterSpec.scala
+++ src/test/scala/FuzzBuzzPrinterSpec.scala
@@ -4,8 +4,7 @@ class FuzzBuzzPrinterSpec extends FlatSpec with Matchers {
 
 
   "FuzzBuzzPrinter" should """print a number for a given number input""" in {
-    val underTest = FuzzBuzzPrinter
-    val output = underTest.getOutput((1 to 15))
+    val output = FuzzBuzzPrinter.getOutput((1 to 15).toList)
     output shouldEqual "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
   }
 
