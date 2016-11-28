f9ee897d99b9efd89c7dacd8d6686cfac745541a
Pavel Lechev <pavel@schoolclubz.com>
2016-11-28 22:06:49 +0000

    "lucky" implemented - test for number containing '3' replaced with "lucky" pass

--- src/main/scala/FuzzBuzzPrinter.scala
+++ src/main/scala/FuzzBuzzPrinter.scala
@@ -8,6 +8,7 @@ object FuzzBuzzPrinter {
 
   def getOutput(range: List[Int]): String = range.map { n =>
     n match {
+      case v if v.toString.contains('3') => "lucky"
       case v if v % 15 == 0 => "fuzzbuzz" // at the top to avoid being caught by 3 & 5 multiples
       case v if v % 3 == 0 => "fuzz"
       case v if v % 5 == 0 => "buzz"
