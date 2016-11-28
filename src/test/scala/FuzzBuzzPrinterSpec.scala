503276a7c8da8cf1f42061f387bd5dfcd5c44407
Pavel Lechev <pavel@schoolclubz.com>
2016-11-28 21:29:51 +0000

    Failing test for "fuzz" replacing multiples of 3

--- src/test/scala/FuzzBuzzPrinterSpec.scala
+++ src/test/scala/FuzzBuzzPrinterSpec.scala
@@ -8,5 +8,10 @@ class FuzzBuzzPrinterSpec extends FlatSpec with Matchers {
     output shouldEqual "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
   }
 
+  it should """print "fuzz" for multiples of 3""" in {
+    val output = FuzzBuzzPrinter.getOutput((1 to 15).toList)
+    output shouldEqual "1 2 fuzz 4 5 fuzz 7 8 fuzz 10 11 fuzz 13 14 fuzz"
+  }
+
 }
 
